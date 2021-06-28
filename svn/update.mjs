d8bd23b843ee77f7f8a24b5dbfc870e17615aff6
plato <plato@whispark.com>
2021-06-28 17:22:01 +0800

    feat: add more tips

--- svn/update.mjs
+++ svn/update.mjs
@@ -2,12 +2,20 @@
 import {promises as fs} from 'fs'
 import path from 'path'
 
-const pid = path.resolve(process.cwd(), 'svn.pid')
+
 $.verbose = false
+const pidPath = path.resolve(process.cwd(), 'svn.pid')
+let pid = await fs.readFile(pidPath)
 const regex = /[0-9]/g;
-let update = await nothrow($`svn update`)
+let update = await $`svn update`
 if (update.exitCode === 0) {
-    fs.writeFile(pid, update.stdout.match(regex).join(''))
+    const newPid = update.stdout.match(regex).join('')
+    if (newPid !== pid) {
+        console.log('update successful')
+    }else {
+        console.log('no update')
+    }
+    fs.writeFile(pidPath, newPid)
 }else {
     console.error(update.stderr)
 }
