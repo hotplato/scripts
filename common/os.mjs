5c5d98b8d673c70203ba1295ede7d9a06bbf22a2
plato <plato@hotplato.com>
2021-06-30 00:18:01 +0800

    feat: add system script

---
+++ common/os.mjs
@@ -0,0 +1,21 @@
+#!/usr/bin/env zx
+
+const commands = Array.from(new Set(process.argv.slice(3)))
+const defaultCommands = {
+  platform: () => os.platform(),
+  type: () => os.type(),
+  version: () => os.version(),
+  arch: () => os.arch(),
+  cpus: () => os.cpus().length,
+  release: () => os.release(),
+  mem: () => '~' + (os.totalmem() / 1024 / 1024 / 1024).toFixed(2) + 'G',
+}
+const output = (c) => {
+  const command = defaultCommands[c]
+  console.log(`${c}: ${command ? command() : ''}\n`)
+}
+if (commands.length) {
+  commands.forEach(output)
+} else {
+  Object.keys(defaultCommands).forEach(output)
+}
